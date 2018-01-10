0d39766248805bf8f41462a1a2c911e0fb8d541f
VenessaV <VenessaV@users.noreply.github.com>
2018-01-09 23:20:32 -0800

    Step 2 in getting to know sc.

--- GettingStartedWithSourceControl/Program.cs
+++ GettingStartedWithSourceControl/Program.cs
@@ -12,6 +12,8 @@ namespace GettingStartedWithSourceControl
         {
             Console.WriteLine("How to commit to source control...");
             Console.WriteLine("1. Chemistry is everything, find your compatible resource.");
+            Console.WriteLine("2. Pay attention to the instruction manual. Or just wing it.");
+            Console.WriteLine("   Either way, copious amounts of time will likely be necessary."); 
             Console.ReadKey();
         }
     }
