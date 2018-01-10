8f379ff1a569dae1b619050c587f9e3b34eb4b40
VenessaV <VenessaV@users.noreply.github.com>
2018-01-09 23:10:06 -0800

    Added commitment step 1

---
+++ GettingStartedWithSourceControl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedWithSourceControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to commit to source control...");
+            Console.WriteLine("1. Chemistry is everything, find your compatible resource.");
+            Console.ReadKey();
+        }
+    }
+}
